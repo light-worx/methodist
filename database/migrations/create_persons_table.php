ed4395db05a2e86f8e47837c38387a37b64588f8
Michael Bishop <michael@bishop.net.za>
2025-12-09 08:16:07 +0200

    Fix migration

--- database/migrations/create_persons_table.php
+++ database/migrations/create_persons_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
             $table->string('title', 199)->nullable();
             $table->string('phone', 199)->nullable();
             $table->string('image', 199)->nullable();
-            $table->integer(`society_id`)->nullable();
-            $table->integer(`circuit_id`)->nullable();
-            $table->json(`leadership`)->nullable();
+            $table->integer('society_id')->nullable();
+            $table->integer('circuit_id')->nullable();
+            $table->json('leadership')->nullable();
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
             $table->timestamp('deleted_at')->nullable();
