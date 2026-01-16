1ba90888f672c9b55fb702b6faaf62d878f962e4
Michael Bishop <michael@bishop.net.za>
2026-01-16 13:23:50 +0200

    Fix preacher add

--- app/Filament/Resources/Societies/RelationManagers/PreachersRelationManager.php
+++ app/Filament/Resources/Societies/RelationManagers/PreachersRelationManager.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Societies\RelationManagers;
 use App\Models\Circuit;
 use App\Models\Person;
 use App\Models\Preacher;
-use App\Models\Society;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\EditAction;
@@ -14,7 +13,6 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
@@ -22,7 +20,6 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Facades\DB;
 
@@ -37,15 +34,9 @@ class PreachersRelationManager extends RelationManager
         return $schema
             ->components([
                 Section::make('Personal details')
+                    ->relationship('person')
                     ->columnSpanFull()
                     ->columns(3)
-                    ->relationship('person', condition: function (string $operation){
-                        if ($operation=="edit"){
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    })
                     ->schema([
                         TextInput::make('firstname')->label('First name')
                             ->required(),
@@ -64,8 +55,8 @@ class PreachersRelationManager extends RelationManager
                             ->tel(),
                         FileUpload::make('image')
                             ->image()
-                    ]),
-                Fieldset::make('Preacher details')
+                        ]),                        
+                Section::make('Preacher details')
                     ->columnSpanFull()
                     ->columns(2)
                     ->schema([
@@ -101,7 +92,6 @@ class PreachersRelationManager extends RelationManager
     {
         return $table
             ->modifyQueryUsing(fn (Builder $query) => $query->with('person'))
-            ->recordTitleAttribute('id')
             ->columns([
                 TextColumn::make('person.surname')
                     ->sortable()
@@ -119,21 +109,78 @@ class PreachersRelationManager extends RelationManager
             ])
             ->headerActions([
                 CreateAction::make()
+                    ->schema([
+                        Section::make('Personal details')
+                            ->columnSpanFull()
+                            ->columns(3)
+                            ->schema([
+                                TextInput::make('person.firstname')->label('First name')
+                                    ->required(),
+                                TextInput::make('person.surname')
+                                    ->required(),
+                                Select::make('person.title')
+                                    ->selectablePlaceholder(false)
+                                    ->options([
+                                        '' => '',
+                                        'Mr' => 'Mr',
+                                        'Ms' => 'Ms',
+                                        'Mrs' => 'Mrs',
+                                        'Rev' => 'Rev'
+                                    ]),
+                                TextInput::make('person.phone')
+                                    ->tel(),
+                                FileUpload::make('person.image')
+                                    ->image()
+                            ]),
+                        Section::make('Preacher details')
+                            ->columnSpanFull()
+                            ->columns(2)
+                            ->schema([
+                                Select::make('leadership')->label('Preacher leadership roles')
+                                    ->multiple()
+                                    ->options(array_combine(
+                                        setting('preacher_leadership_roles'),
+                                        setting('preacher_leadership_roles')
+                                    )),
+                                Select::make('status')
+                                    ->live()
+                                    ->options([
+                                        'note' => 'Preacher on note',
+                                        'trial' => 'Preacher on trial',
+                                        'preacher' => 'Local preacher',
+                                        'emeritus' => 'Emeritus preacher',
+                                        'guest' => 'Guest preacher'
+                                    ]),
+                                TextInput::make('number')->label('Preacher number (optional)')
+                                    ->numeric(),
+                                TextInput::make('induction')->label('Year of induction')
+                                    ->readonly(function (Get $get){
+                                        return !in_array($get('status'), ['preacher', 'emeritus']);
+                                    }),
+                                Toggle::make('active')
+                                    ->onColor('success'),
+                        ]),
+                    ])
                     ->using(function (array $data, RelationManager $livewire): Preacher {
                         $society = $livewire->getOwnerRecord();
-                        dd($data['person']);
                         $person = Person::create([
-
+                            'firstname' => $data['person']['firstname'],
+                            'surname' => $data['person']['surname'],
+                            'title' => $data['person']['title'],
+                            'phone' => $data['person']['phone'],
+                            'image' => $data['person']['image']
                         ]);
-                        $status=array();
                         $preacher = $person->preacher()->create([
                             'person_id' => $person->id,
                             'society_id' => $society->id,
-                            'status' => $data['preacherstatus'],
-                            'active' => 1
+                            'status' => $data['status'],
+                            'leadership' => json_encode($data['leadership']),
+                            'number' => $data['number'],
+                            'induction' => $data['induction'],
+                            'active' => $data['active']
                         ]);
                         $circuit=Circuit::find($society->circuit_id);
-                        if ($person->status=="guest"){
+                        if ($preacher->status=="guest"){
                             $status[]="Guest";
                         } else {
                             $status[]="Preacher";
