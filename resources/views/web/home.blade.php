99d42629967a28fd10115e5b7c740ad9662caeb0
Michael Bishop <michael@bishop.net.za>
2026-03-13 20:29:30 +0200

    Updated home page

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,4 +1,6 @@
 <x-web pageName="MCSA Connexion">
+    This site has been updated. For a time, the old site will be available at <a href="https://mcsa.church.net.za">https://mcsa.church.net.za</a>, but please explore the new site! If you were an authorised user on the previous system, please <a href="{{ url('/admin') }}">log in</a> from the menu using your old credentials and let us know if you have any issues.<br><br>
+
     <ul class="list-unstyled">
         @foreach ($districts as $district)
             @if ($district->active)
