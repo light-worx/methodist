2f7b1713629245fbe9240fec58c4c1d35d027f2b
Michael Bishop <michael@bishop.net.za>
2026-04-15 17:58:35 +0200

    Password reset

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->topNavigation()
+            ->passwordReset()
             ->login()
             ->colors([
                 'primary' => Color::Red,
